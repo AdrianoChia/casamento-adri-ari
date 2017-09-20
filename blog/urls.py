c9fa4644e50ed97a279cd0eac8c03a95a2bffad1
admin <adriano.chiamulera@gmail.com>
2017-09-19 22:26:03 -0300

    Transformado no site de casamento

--- blog/urls.py
+++ blog/urls.py
@@ -4,12 +4,11 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
-    url(r'^post/new/$', views.post_new, name='post_new'),
-    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
-    url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
-    url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
-    url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
-
+    url(r'^$', views.home, name='home'),
+    url(r'^home/', views.home, name='home'),
+    url(r'^contato/', views.contato, name='contato'),
+    url(r'^evento/', views.evento, name='evento'),
+    url(r'^galeria/', views.galeria, name='galeria'),
+    url(r'^lista_presentes/', views.lista_presentes, name='lista_presentes'),
+    url(r'^rsvp/', views.rsvp, name='rsvp'),
 ]
