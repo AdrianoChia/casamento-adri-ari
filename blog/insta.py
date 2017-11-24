afb22f1ca28ad49ee95ddc0fb7a4890eec17e88a
unknown <adriano.chiamulera@gmail.com>
2017-11-24 10:50:39 -0200

    Resumo do método do insta

--- blog/insta.py
+++ blog/insta.py
@@ -1,25 +1,16 @@
 import json
 import requests
+import dropbox
+import urllib
 
-tag_recent = 'https://api.instagram.com/v1/tags/trancoso/media/recent?access_token=6361071795.c966c6f.1e5c3710bf764d3da3ba52bc66b78884'
+tag_recent = 'https://api.instagram.com/v1/tags/becodobatman/media/recent?access_token=6361071795.c966c6f.1e5c3710bf764d3da3ba52bc66b78884'
 r = requests.get(tag_recent)
-if r.status_code == 200:
-    photo = json.loads(r.content)
-    photo_url = (photo['data'][0]['images']['standard_resolution']['url'])
-else: 
-    print ('Erro na conexão, verificar token')
-
-'''        
-user_profile = 'https://api.instagram.com/v1/users/6361071795/?access_token=6361071795.c966c6f.1e5c3710bf764d3da3ba52bc66b78884'
-r = requests.get(user_profile)
-if r.status_code == 200:
-    recent_media = json.loads(r.content)
-    recent_media = (recent_media['data'][0]['images']['standard_resolution']['url'])
-
-user_recent = 'https://api.instagram.com/v1/users/6361071795/media/recent/?access_token=6361071795.c966c6f.1e5c3710bf764d3da3ba52bc66b78884'
-r = requests.get(user_recent)
-if r.status_code == 200:
-    recent_media = json.loads(r.content)
-    profile_picture =  (user['data']['profile_picture'])
-
-, 'recent_media': recent_media, 'profile_picture': profile_picture) '''
+photo = json.loads(r.content)
+foto = photo['data'][0]['images']['standard_resolution']['url']
+urllib.request.urlretrieve(foto, 'img/teste.jpg')
+access_token = 'UQ0p70bOHWAAAAAAAAAADUwHjPwgPCOsI7p6-yMDF0g-LV4C4_lTiuRgfSdkvazw'
+file_from = 'img/teste.jpg'
+file_to = '/Fotos/teste.jpg'
+dbx = dropbox.Dropbox(access_token)
+f = open(file_from, 'rb')
+dbx.files_upload(f.read(), file_to)
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
 
+import json
+import requests
+import dropbox
+
 # Create your tests here.
+def tag(request):
+	tag_recent = 'https://api.instagram.com/v1/tags/becodobatman/media/recent?access_token=6361071795.c966c6f.1e5c3710bf764d3da3ba52bc66b78884'
+	r = requests.get(tag_recent)
+	if r.status_code == 200:
+		photo = json.loads(r.content)
+		n = 0
+		m = 0
+		a = [photo['data'][n]['carousel_media'][m]]
+		foto = {}
+		while (n <= 100):
+			if not a:
+				while (n <= 100):
+					foto = {photo['data'][n]['images']['standard_resolution']['url']}
+					n = n + 1
+			else:
+				while (m <= 100):
+						foto = photo['data'][n]['carousel_media'][m]['images']['standard_resolution']['url']
+						m = m + 1
+			n = n + 1
+	else:
+		print ('Erro na conexão, verificar token')
+	return ({'foto": foto'})	
+#	return render(request, 'blog/tag.html', {'foto': foto})
+
+
+def dropbox(request):
+	dbx  = dropbox.Dropbox('UQ0p70bOHWAAAAAAAAAADUwHjPwgPCOsI7p6-yMDF0g-LV4C4_lTiuRgfSdkvazw')
+	dbx.users_get_current_account()
+
+import dropbox
+
+access_token = 'UQ0p70bOHWAAAAAAAAAADUwHjPwgPCOsI7p6-yMDF0g-LV4C4_lTiuRgfSdkvazw'
+file_from = 'img/teste.jpg'  //local file path
+file_to = '/Fotos/teste.jpg'  // dropbox path
+
+def upload_file(file_from, file_to):
+    dbx = dropbox.Dropbox(access_token)
+    f = open(file_from, 'rb')
+    dbx.files_upload(f.read(), file_to)
+upload_file(file_from,file_to)
